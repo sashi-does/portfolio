848bb192d0ad29b959cacad50f8cc6ce860a9351
sashi-does <sashiontop@gmail.com>
2025-05-19 00:55:05 +0530

    about: sider fixed

--- src/components/AboutSidebar.tsx
+++ src/components/AboutSidebar.tsx
@@ -12,9 +12,9 @@ const sidebarItems = [
           {sidebarItems.map((item, index) => (
             <li
               key={index}
-              className="group flex items-center space-x-2 cursor-pointer transition-all duration-300 hover:text-blue-400"
+              className="group flex items-center space-x-2 cursor-pointer transition-all duration-300 hover:text-[#15C0FD]"
             >
-              <div className="w-4 h-[1px] bg-gray-500 group-hover:bg-blue-400 transition-all duration-300" />
+              <div className="w-4 h-[1px] bg-gray-500 group-hover:bg-[#15C0FD] transition-all duration-300" />
               <p className="text-lg transform transition-transform duration-300 group-hover:translate-x-1">
                 {item}
               </p>
